f65c5800c1c2b5386043a17e82aaa67d487021a3
Edu <edujaguan@gmail.com>
2023-12-17 17:27:03 +0100

    hy

--- descomprime.asm
+++ descomprime.asm
@@ -1,84 +1,105 @@
-Descomprime:        ld r20, r30, 0 ;guardamos en r20 com (texto comprimido)
-                    or r10, r0, r0 ;contador de la posicion en texto comprimido
-                    addu r12, r0, 5 ;contador de la posicion en el mapa de bits  --- mantenemos un unico puntero (r20) y los accesos si es al texto los hacemos sumando r10 y si es al mapa de bits sumando r12
+Descomprime:        ld r20, r30, 0 ;cargamos en r20 el puntero a com 
+                    or r10, r0, r0 ;offset a posicion en com
+                    or r12, r0, 5 ;offset mapa de bits
 
-                    ld r21, r30, 4 ;guardamos en r21 desc (zona donde guardar textp descomprimido)
-                    or r11, r0, r0 ;contador de la posicion en la zona de texto descomprimido
+                    ld r21, r30, 4 ;cargamos en r21 el puntero a desc
+                    or r11, r0, r0 ;offset en desc
 
-                    addu r9, r0, 8 ;contador de los 8 primeros bytes
-
-                    ld.bu r29,r20 ,0 ;Iniciamos el byte menos significativo de r29 con la longitud de la cadena descomprimida
-                    ld.bu r15, r20, 1 ;cargamos en r15 el byte de r20
-                    mulu r15, r15, 256 ;desplazamos el byte ,menos significativo para que ahora sea el segundo menos significativo
-                    or r29, r29, r15 ;metemos el segundo byte de r15 en el segundo byte menos significativo de r29
-
-                    ld.bu r8, r20, 3;cargamos en r8 el byte menos significativo de la posocion donde comienza el texto comprimido
-                    ld.bu r7, r20, 4 ;misma operacion de desplazamiento que arriba
-                    mulu r7, r7, 256 
-                    or r8, r8, r7
-
-                    or r10, r8, r8 ;r10 ahora apunta a la zona donde comiena el texto comprimido
-                    or r2, r0, 7 ;contador de bits de cada byte
+                    ld.bu r29, r20, 0 ;primer byte de la cabecera
+                    ld.bu r15, r20, 1 ;segundo byte de la cabecera 
+                    mulu r15, r15, 256 ;desplazamos r15
+                    or r29, r29, r15 ;r29 <-- priemros 2 bytes de la cabecera
 
 
-buc8xM_des:         ld.bu r15, r20, r10 ;cargamos el byte del texto en r15
-                    cmp r5, r9, r0 ;comparamos si hemos copiado ya los 8 primeros bytes
-                    bb1 eq, r5, buc_byte ;si ya los hemos copiado salimos del bucle
-                    st.b r15, r21, r11 ;guardamos el byte en la zona de texto descomprimido
-                    addu r10, r10, 1
+                    ld.bu r13, r20, 3
+                    ld.bu r14, r20, 4
+                    mulu r14, r14, 256
+                    or r10, r13, r14 ;r10 es el offset de la cadena com
+
+                    or r8, r0, 8
+                    or r2, r0, 7
+
+buc8xM_des:         ld.bu r15, r20, r10
+                    cmp  r5, r8, 0
+                    bb1 eq, r5, buc_byte
+                    st.b r15, r21, r11
                     addu r11, r11, 1
-                    subu r9, r9, 1 ;restamos al contador
-                    br buc8xM_des
+                    addu r10, r10, 1
+                    subu r8, r8, 1
+                    br buc8xM_des     
 
-buc_byte:           cmp r5, r11, r29 ;comparamos si hemos reccorido todos los bytes del texto descomprimido
-                    bb1 eq, r5, fin_des
+buc_byte:           cmp  r5, r11, r29
+                    bb1 ht, r5, fin_des
 
-                    ;logica de ver que valor tiene cada bit (0 o 1) y hacer lo que corresponda
                     cmp r5, r2, 7
                     bb1 eq, r5, pos7
-                    br bit0
 
                     cmp r5, r2, 6
                     bb1 eq, r5, pos6
-                    br bit0
 
                     cmp r5, r2, 5
                     bb1 eq, r5, pos5
-                    br bit0
 
                     cmp r5, r2, 4
                     bb1 eq, r5, pos4
-                    br bit0
 
                     cmp r5, r2, 3
                     bb1 eq, r5, pos3
-                    br bit0
 
                     cmp r5, r2, 2
                     bb1 eq, r5, pos2
-                    br bit0
 
                     cmp r5, r2, 1
                     bb1 eq, r5, pos1
-                    br bit0
 
-                    ;si estamos en la ultima posicion (pos0)----
+                    ;pos0
                     or r16, r15, 1
-                    addu r2,r0, 7
-                    ld.bu r15, r20, r12 ;cargamos el byte del mapa de bits en r15
-                    addu r12, r12, 1 ;apuntamos al siguiente byte
+                    or r2, r0, 7
                     cmp r5, r16, r15
                     bb1 eq, r5, bit1
-                    ;el bit es 0
-                    br bit0
+                    br bit0  
+pos7:               ld.bu r15, r20, r12 ;cargamos en r15 el byte del mapa de bits
+                    addu r12, r12, 1
+                    or r16, r15, 128
+                    subu r2, r2, 1
+                    cmp r5, r16, r15
+                    bb1 eq, r5, bit1
+                    br bit0  
+pos6:               or r16, r15, 64
+                    cmp r5, r16, r15
+                    bb1 eq, r5, bit1
+                    br bit0  
+pos5:               or r16, r15, 32
+                    subu r2, r2, 1
+                    cmp r5, r16, r15
+                    bb1 eq, r5, bit1
+                    br bit0  
+pos4:               or r16, r15, 16
+                    subu r2, r2, 1
+                    cmp r5, r16, r15
+                    bb1 eq, r5, bit1
+                    br bit0  
+pos3:               or r16, r15, 8
+                    subu r2, r2, 1
+                    cmp r5, r16, r15
+                    bb1 eq, r5, bit1
+                    br bit0  
+pos2:               or r16, r15, 4
+                    subu r2, r2, 1
+                    cmp r5, r16, r15
+                    bb1 eq, r5, bit1
+                    br bit0  
+pos1:               or r16, r15, 2
+                    subu r2, r2, 1
+                    cmp r5, r16, r15
+                    bb1 eq, r5, bit1
+                    br bit0  
 
-                    
 bit0:               ld.bu r17, r20, r10 ;guardamos en r17 el siguiente caracter del texto comprimido
                     st.b r17, r21, r11 ;guardamos en le texto descomprimido el caracter
                     addu r11, r11, 1 ;aumentamos punteros para apuntar a los siguientes bytes
                     addu r10, r10, 1
-                    br buc_byte
-
+                    br buc_byte         
 bit1:               ld.bu r18, r20, r10 ;guardamos en r18 el siguiente byte del texto comprimido
                     addu r10, r10, 1 ;incrementamos el offset para ahora leer el siguiente byte de P
                     ld.bu r19, r20, r10 ;leemos el segundo byte de P
@@ -96,37 +117,8 @@ Bucle_copia:        cmp r5, r18, r0 ;verificamos si hemos leido los L elementos
                     addu r19, r19, 1
                     subu r18, r18, 1 
 
-                    br Bucle_copia         
+                    br Bucle_copia   
 
-pos7:               or r16, r15, 128
-                    subu r2, r2, 1
-                    cmp r5, r16, r15
-                    bb1 eq, r5, bit1  
-pos6:               or r16, r15, 64
-                    subu r2, r2, 1
-                    cmp r5, r16, r15
-                    bb1 eq, r5, bit1  
-pos5:               or r16, r15, 32
-                    subu r2, r2, 1
-                    cmp r5, r16, r15
-                    bb1 eq, r5, bit1  
-pos4:               or r16, r15, 16
-                    subu r2, r2, 1
-                    cmp r5, r16, r15
-                    bb1 eq, r5, bit1  
-pos3:               or r16, r15, 8
-                    subu r2, r2, 1
-                    cmp r5, r16, r15
-                    bb1 eq, r5, bit1  
-pos2:               or r16, r15, 4
-                    subu r2, r2, 1
-                    cmp r5, r16, r15
-                    bb1 eq, r5, bit1  
-pos1:               or r16, r15, 2
-                    subu r2, r2, 1
-                    cmp r5, r16, r15
-                    bb1 eq, r5, bit1  
-
-
-fin_des:            st.b r0, r21, r11 ;almacenar '/0' al final de la cadena descomprimida
-                    jmp(r1)
+fin_des:                st.b r0, r21, r11 ;guardamos caracter nulo
+                    jmp(r1)  
+     
